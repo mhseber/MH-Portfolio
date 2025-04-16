af77e4478174a95f09ae96a15cadf6bacdeeb5ae
mahamudul hasan seber <sebermh@gmail.com>
2025-04-16 15:47:47 +0600

    Routes SetUp Done

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Footer from "../Shared/Footer";
+
+const Main = () => {
+  return (
+    <div>
+      {" "}
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default Main;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,3 @@
-// /** @type {import('tailwindcss').Config} */
-// import daisyui from "daisyui"
-// export default {
-//   content: [
-//     "./index.html",
-//     "./src/**/*.{js,ts,jsx,tsx}",
-//   ],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [daisyui],
-// }
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
